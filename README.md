b5e2324d9cbbbc41cbc5b869de4eb1ea66237e1f
Nadezhda <32543510+Svobodinang@users.noreply.github.com>
2022-12-03 12:42:30 +0300

    Add goods and button components

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ecco-base-with-components
 Базовый простой проект с компонентами для ecco
+
+# чтобы запустить проект
+npm i
+npm run dev
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import './app.scss';
+
+import Swipers from './js/swipers';
+
+function start() {
+    // some js here
+    // eslint-disable-next-line
+    const swipers = new Swipers();
+}
+
+start();
